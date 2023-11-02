00af7a525ab55850c4aecc342aff7767750db52d
SUJITH T S <99735954+07SUJITH@users.noreply.github.com>
2023-11-02 09:50:33 +0530

    Update pass1of2pass.c

--- pass1of2pass.c
+++ pass1of2pass.c
@@ -2,13 +2,85 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include<stdbool.h>
 
-struct SYMTAB{
-    char symbol[30];
-    char address[30];
-} s[100];
+void display();  
+void onepass();   
+int hexToDecimal( char *);  
 
-int scount = 0;
+int  main(){
+    onepass();
+    return 0;
+}
+
+void onepass(){
+    char label[30], opcode[30], operand[30];
+    bool validOpcode;
+    char code[10],mnemonic[10];
+    int startAddress, locctr = 0, i;
+    FILE *fp1,*fp2,*fp3,*fp4,*fp5;
+    fp1 = fopen("input.txt", "r");
+    fp2 =fopen("optab.txt","r");
+    fp3 =fopen("symtab.txt","w");
+    fp4 = fopen("intermediate.txt", "w");
+    fp5 = fopen("length.txt","w");
+    fscanf(fp1, "%s%s%s", label, opcode, operand);
+ 
+    if (strcmp(opcode, "START") == 0){
+            startAddress = hexToDecimal(operand);
+            locctr = startAddress;
+            fprintf(fp4, "%x\t\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
+            fscanf(fp1, "%s%s%s", label, opcode, operand);
+    }else locctr = 0;
+
+    while (strcmp(opcode,"END") !=  0){
+        fprintf(fp4, "%x\t\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
+        if (strcmp(label, "**") != 0){ 
+            fprintf(fp3,"%s\t%x\n",label,locctr);
+        }
+        fscanf(fp2, "%s\t%s", code, mnemonic);
+        validOpcode =false;
+        while (strcmp(code, "END") != 0) {
+            if (strcmp(opcode, code) == 0) {                
+                locctr += 3;
+                validOpcode = true;
+                break;
+            }
+            fscanf(fp2, "%s\t%s", code, mnemonic);
+        }
+        if(!validOpcode){
+            if (strcmp(opcode, "WORD") == 0)
+        		 	locctr += 3;
+            else if (strcmp(opcode, "RESW") == 0)
+        	 	locctr += atoi(operand) * 3;
+            else if (strcmp(opcode, "RESB") == 0)
+        	  	locctr += atoi(operand);
+            else if (strcmp(opcode, "BYTE") == 0){
+                int len = strlen(operand);
+                len -=3;
+                if(operand[0] == 'C' || operand[0] == 'c')
+                    locctr += len;
+                else    
+                    locctr += ceil(len/2.0);
+            }
+            else{
+                printf("\ninvalid opcode %s  !!!",opcode);
+                exit(1);
+            }    
+        }
+        fscanf(fp1, "%s%s%s", label, opcode, operand); 
+    }
+    fprintf(fp4, "%x\t\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
+    int programLength = locctr - startAddress;
+    fprintf(fp5, "%x",programLength);
+    fclose(fp1);    
+    fclose(fp2);    
+    fclose(fp3);
+    fclose(fp4);
+    fclose(fp5);
+    display();
+}
+
 int hexToDecimal(char hex[]){
     int len, i, temp;
     int dec = 0;
@@ -18,90 +90,54 @@ int hexToDecimal(char hex[]){
         	temp = hex[i] - '0';
         else 
         	temp = hex[i]  - '0' - 7 ;
-        	
         dec = dec + temp * pow(16, len - i - 1);
     }
     return dec;
 }
-void onepass(){
-    char label[30], opcode[30], operand[30], address[30];
-    char loc[30];
-    int flag, j;
-    int startaddress, locctr = 0, i;
-    FILE *fp1, *fp2,*fp3,*fp4;
-    fp1 = fopen("source.txt", "r");
-    fp2 = fopen("intermediate.txt", "w");
-    fscanf(fp1, "%s%s%s", label, opcode, operand);
-    while (!feof(fp1)){
-        if (strcmp(opcode, "START") == 0){
-            startaddress = hexToDecimal(operand);
-            locctr = hexToDecimal(operand);
-            fprintf(fp2, "%x\t\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
-            fscanf(fp1, "%s%s%s", label, opcode, operand);
-        }
-        if (strcmp(label, "**") != 0){
-            strcpy(s[scount].symbol, label);
-            sprintf(address, "%x", locctr);
-            strcpy(s[scount++].address, address);
-        }
-        fprintf(fp2, "%x\t\t%s\t\t%s\t\t%s\n", locctr, label, opcode, operand);
-        if (strcmp(opcode, "WORD") == 0)		 	locctr += 3;
-        else if (strcmp(opcode, "RESW") == 0)	 	locctr += atoi(operand) * 3;
-        else if (strcmp(opcode, "RESB") == 0)	  	locctr += atoi(operand);
-        else if (strcmp(opcode, "BYTE") == 0) 		locctr += strlen(operand);
-        else	if (strcmp(opcode,"END") !=0)			locctr += 3;
-        fscanf(fp1, "%s%s%s", label, opcode, operand);
-    }
-    fclose(fp2);
-    fclose(fp1);
-    
-    printf("symbol table\n\n");
-    fp4 =fopen("symtab.txt","w");
-    for (i = 0; i < scount; i++){   
-    	printf("%s\t%s\n", s[i].symbol, s[i].address);
-    	fprintf(fp4,"%s\t%s\n",s[i].symbol,s[i].address);
-    }
-    fclose(fp4);
+void display(){
+    FILE *fp;
+    printf("SYMBOL TABLE\n\n");
+    fp = fopen("symtab.txt","r");
+    char ch = fgetc(fp);
+    while(ch != EOF){
+        printf("%c",ch);
+        ch = fgetc(fp);
+    }    
+    fclose(fp);
 
-    printf("\nintermediate code of assembler \n\n");
-    fp2 = fopen("intermediate.txt", "r");
-    char str = fgetc(fp2);
-    while (str != EOF) {
-        printf("%c", str);
-        str = fgetc(fp2);
+    printf("\nINTERMEDIATE FILE \n\n");
+    fp = fopen("intermediate.txt", "r");
+    ch = fgetc(fp);
+    while (ch != EOF) {
+        printf("%c", ch);
+        ch = fgetc(fp);
     }
-    fclose(fp2);
-
-    fp3 = fopen("length.txt","w");
-    int length = locctr - startaddress;
-    printf("\nlength of Program : %x\n",length);
-    fprintf(fp3, "%x",length);
-    fclose(fp3);
+    fclose(fp);
+    fp = fopen("length.txt","r");
+    char length[5] ;
+    fscanf(fp,"%s",length);
+    printf("\nPROGRAM LENGTH : %s bytes \n\n",length);
+    fclose(fp);
 }
 
-void main(){
-    onepass();
-}
 /*
-symbol table
+SYMBOL TABLE
 
-FIRST	1000
-ALPHA	1009
-BETA	100c
-GAMMA	100f
+FIRST   1000
+ALPHA   1009
+BETA    100c
+GAMMA   100f
 
-intermediate code of assembler 
+INTERMEDIATE FILE
 
-1000		COPY		START		1000
-1000		FIRST		LDA		ALPHA
-1003		**		MUL		BETA
-1006		**		STA		GAMMA
-1009		ALPHA		WORD		2
-100c		BETA		WORD		4
-100f		GAMMA		RESW		1
-1012		**		END		1000
+1000            COPY            START           1000
+1000            FIRST           LDA             ALPHA
+1003            **              MUL             BETA
+1006            **              STA             GAMMA
+1009            ALPHA           WORD            2
+100c            BETA            WORD            4
+100f            GAMMA           RESW            1
+1012            **              END             1000
 
-length of Program : 12
-
-
+PROGRAM LENGTH : 12 bytes
 */
